072735fd56b67a70e0757f640fb1c19e11406e93
Ravin <fabprojectmanager@gmail.com>
2025-07-16 19:18:44 +0530

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <div>
       <Banner />
-        <Trusted />
+      <Trusted />
       <MultiColumn />
       <Services />
       <ImageWithText />
