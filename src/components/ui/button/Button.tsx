fa38a08939295b67fb4d2da433e634bb73f8cec2
Ravin <fabprojectmanager@gmail.com>
2025-07-16 19:00:49 +0530

    added footer

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -2,10 +2,11 @@
 import { ButtonType } from "@/types";
 import React from "react";
 
-const Button = ({ className, label = "Get Started", onClick }: ButtonType) => {
+const Button = ({ className, label = "Get Started", onClick, type }: ButtonType) => {
   return (
     <button
       onClick={onClick}
+      type={type}
       className={`text-center text-white text-xl font-medium font-inter leading-[1.2] bg-accent-100 py-[18px] px-11 rounded-4xl ${
         className || ""
       }`}
